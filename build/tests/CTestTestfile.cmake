9d8077ae37d8519973925489acd39d1d812ad438
IdanKoblik <me@idank.dev>
2026-01-30 22:39:18 +0200

    remove build dir

--- build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/idan/projects/personal/kutiles/tests
-# Build directory: /home/idan/projects/personal/kutiles/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[hash_table_tests]=] "/home/idan/projects/personal/kutiles/build/tests/test_hash_table")
-set_tests_properties([=[hash_table_tests]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/idan/projects/personal/kutiles/tests/CMakeLists.txt;7;add_test;/home/idan/projects/personal/kutiles/tests/CMakeLists.txt;0;")
